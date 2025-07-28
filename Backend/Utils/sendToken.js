05b60edcaf0ea901cd5d0457bb6920ea21ba8325
PatelSnehV <snehpatel1233@gmail.com>
2025-07-28 10:00:48 +0530

    final

--- Backend/Utils/sendToken.js
+++ Backend/Utils/sendToken.js
@@ -1,16 +1,19 @@
 export const sendToken = function (user, statusCode, message, res) {
   const token = user.generateToken();
 
-  res
-    .status(statusCode)
-    .cookie("token", token, {
-      expires: new Date(Date.now() + 24 * 60 * 60 * 1000 * 3), // 3 days
-      httpOnly: true,
-    })
-    .json({
-      success: true,
-      user,
-      message,
-      token,
-    });
+res
+  .status(statusCode)
+  .cookie("token", token, {
+    expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000), // 3 days
+    httpOnly: true,
+    sameSite: "None", // ← important for cross-site cookies
+    secure: true      // ← required for "SameSite=None"
+  })
+  .json({
+    success: true,
+    user,
+    message,
+    token
+  });
+
 };
